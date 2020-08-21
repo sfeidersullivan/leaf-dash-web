83863a428e595f2a7904fb30e77928001415cab7
Seamus Feider-Sullivan <seamus@Seamuss-MacBook-Pro.local>
2020-08-21 15:01:34 -0700

    update home page layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,19 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import DataLog from "../components/dataLog"
+import DataLog from "../components/DataLog"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <DataLog />
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <DataLog variable="temperature" yMin={65} yMax={90} title="Temperature" />
+    <DataLog variable="humidity" yMin={0} yMax={100} title="Humidity" />
+    { false && <>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </>}
   </Layout>
 )
 
